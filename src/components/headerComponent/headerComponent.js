15ccc6e8796aed3cd8ab889577456831e34bb56c
Quintessa Anderson <quintessa@8thlight.com>
2019-04-16 17:19:31 -0500

    Cleaned up app, removed header

--- src/components/headerComponent/headerComponent.js
+++ src/components/headerComponent/headerComponent.js
@@ -6,15 +6,6 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <div className="jumbotron">
-                <h1 className="display-4 App-intro">Hello, React Enthusiasts!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Click to Play</a>
-                </p>
-            </div>
             <div className="App-header">
                 <h2>Welcome to React Tic Tac Toe</h2>
 
