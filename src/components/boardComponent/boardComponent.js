457d814643a52157f6f0233bf14f82c3d5a177b8
Quintessa Anderson <quintessa@8thlight.com>
2019-04-22 13:36:59 -0500

    updated test suite

--- src/components/boardComponent/boardComponent.js
+++ src/components/boardComponent/boardComponent.js
@@ -3,30 +3,25 @@ import Square from '../squareComponent/squareComponent';
 
 class Board extends Component {
 
-    renderSquare(i) {
-        /* Passing prop from this parent to child component */
-        return <Square value={i} /> 
-    }
-
     render() {
         return (
             <div className="Board Board_Row">
                 <div className="text instructions-text">
                     <div className="row">
                         <div className="board-grid col-lg-4 col-md-4 col-sm-4">
-                            {this.renderSquare(0)}  {/*Invokes the renderSquare method */}
-                            {this.renderSquare(1)}
-                            {this.renderSquare(2)}
+                            <Square />
+                            <Square />
+                            <Square />
                         </div>
                         <div className="board-grid col-lg-4 col-md-4 col-sm-4">
-                            {this.renderSquare(3)}  {/*Invokes the renderSquare method */}
-                            {this.renderSquare(4)}
-                            {this.renderSquare(5)}
+                            <Square />
+                            <Square />
+                            <Square />
                         </div>
                         <div className="board-grid col-lg-4 col-md-4 col-sm-4">
-                            {this.renderSquare(6)}  {/*Invokes the renderSquare method */}
-                            {this.renderSquare(7)}
-                            {this.renderSquare(8)}
+                            <Square />
+                            <Square />
+                            <Square />
                         </div>
                     </div>
                 </div>
--- src/components/squareComponent/squareComponent.js
+++ src/components/squareComponent/squareComponent.js
@@ -5,7 +5,8 @@ class Square extends Component {
     constructor(props) {
         super(props) // call super parent class constructor props
         this.state = {
-            showMarker: true, // if true then marker will be shown    
+            showMarker: true, // if true then marker will be shown  
+            xValue: null  
         } 
     }
 
@@ -14,6 +15,7 @@ class Square extends Component {
         const { showMarker } = this.state;
         this.setState({showMarker});
         console.log("Square Button Clicked!"); 
+        this.setState({xValue: 'X'})
         console.log(this.state);
     }
 
@@ -22,8 +24,7 @@ class Square extends Component {
             <div className="Square">
 
                 <div>
-                    {/*Passing prop to child component */}
-                    <button type="button" onClick={this.toggleSquare} className="btn btn-outline-primary board-Square">{this.props.value}</button> 
+                    <button type="button" onClick={this.toggleSquare} className="btn btn-outline-primary board-Square">{this.state.xValue}</button> 
 
                 </div>
 
