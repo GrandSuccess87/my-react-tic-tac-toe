36a7be74edbec141135c60492aa6103db24e856d
Quintessa Anderson <quintessa@8thlight.com>
2019-04-22 14:47:15 -0500

    Updated test suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Header from './components/mainComponent/mainComponent';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
