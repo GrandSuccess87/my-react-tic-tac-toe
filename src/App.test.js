de2f84f7463524d6909fc6799f9ec8536a58d354
Quintessa Anderson <quintessa@8thlight.com>
2019-04-25 11:52:40 -0500

    used contains to pass tests

--- src/App.test.js
+++ src/App.test.js
@@ -24,17 +24,14 @@ it('shows loads game component without crashing', () => {
 it('defaults to not rendering the board', () => {
   const wrapper = shallow(<Game />);
   const board = wrapper.find(Board);
-
-  // expect(wrapper.contains(<Board />)).toBe(false);
-  expect(board.length).toEqual(0);
-  
+  expect(wrapper.contains(<Board />)).toBe(false);
 });
 
 it('renders the board after the start button is clicked', () => {
   const wrapper = shallow(<Game />);
   wrapper.find('button').simulate('click');
   const board = wrapper.find(Board);
-  expect(board.length).toEqual(1);
+  expect(wrapper.contains(<Board />)).toBe(true);
 });
 
 it('asserts false', () => {
