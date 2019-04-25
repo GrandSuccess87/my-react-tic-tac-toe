00a51f785e01de8f63b83de9f3844b0a7a75e7c0
Quintessa Anderson <quintessa@8thlight.com>
2019-04-25 11:58:01 -0500

    pushing code working code and tests for show board, ready for review to merge

--- src/components/gameComponent/gameComponent.js
+++ src/components/gameComponent/gameComponent.js
@@ -9,33 +9,24 @@ class Main extends Component {
         super(); 
         this.state = {
             show: false,
-            disabled: false // button will not be disabled
         }; 
     }
 
     toggleBoard = (e) => {
         const { show } = this.state;
-        this.setState({show: !show}) 
-
-    }
-
-    handleClick = () => {
-        // button will be disabled
-        this.setState({disabled: !this.state.disabled});
-    }
+        this.setState({show: !show});
+    };
 
     render() {
         return (
             <div>
-            <div className="App-header">
-                <h2>Welcome to React Tic Tac Toe</h2>
-                <img src={logo} className="App-logo" alt="logo" /> <br />
-                <div className="start_game">
-                <button type="button" onClick={this.toggleBoard} className="btn btn-outline-primary">Start Game</button>
-                {this.state.show ? <Board /> : <span /> }
-                
-
-                </div>
+                <div className="App-header">
+                    <h2>Welcome to React Tic Tac Toe</h2>
+                    <img src={logo} className="App-logo" alt="logo" /> <br />
+                    <div className="start_game">
+                        <button type="button" onClick={this.toggleBoard} className="btn btn-outline-primary">Start Game</button>
+                        {this.state.show ? <Board /> : <span /> }
+                    </div>
                 </div>
           </div>
         );
