5401442d0a56ccf41f0b4d9f2cdd03450c2d62e3
Quintessa Anderson <quintessa@8thlight.com>
2019-05-03 14:17:26 -0500

    Refactor pages directory
     * Introduce index.js to export the Pages namespace
     * Introduce page for Main

---
+++ src/pages/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from '../components/boardComponent/boardComponent';
+
+export default class GamePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <Game />
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Main from "../components/mainComponent/mainComponent";
+
+export default class MainPage extends React.Component {
+
+    render() {
+        return <Main />;
+    }
+}
