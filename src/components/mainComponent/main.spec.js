d56c0d6b331d4bd39aed8776856c40428a4cdd80
Quintessa Anderson <quintessa@8thlight.com>
2019-05-03 14:14:08 -0500

    Basic cleanup to main component
     * Fix whitespace issues
     * Remove unneccessary test

--- src/components/mainComponent/main.spec.js
+++ src/components/mainComponent/main.spec.js
@@ -1,22 +1,17 @@
+import Enzyme from "enzyme";
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Main from './mainComponent';
-import Enzyme from 'enzyme';
 import {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Main Component', () => {
-    it('renders Main component without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<Main />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });    
-
     it('renders a new page after start button is click', () => {
         const wrapper = shallow(<Main />);
+
         wrapper.find('a').simulate('click');
+
         expect(wrapper.find('[href="./game"]').length).toBe(1);
-      });
+    });
 });
--- src/components/mainComponent/mainComponent.js
+++ src/components/mainComponent/mainComponent.js
@@ -8,11 +8,13 @@ class Main extends Component {
         return (
             <div>
                 <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" /> <br />
-                        <h2>Welcome to React Tic Tac Toe</h2><br/>
-                            <div className="start_game">
-                                <a className="game-page" href="./game"><span><div id="start">Start Game</div></span></a>
-                            </div>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <br />
+                    <h2>Welcome to React Tic Tac Toe</h2>
+                    <br/>
+                    <div className="start_game">
+                        <a className="game-page" href="./game"><span><div id="start">Start Game</div></span></a>
+                    </div>
                 </div>
           </div>
         );
